93184b05370f59d7aa9cf68d54e870d7d9153153
Andrey Danin <danindrey@mail.ru>
2015-02-27 15:02:44 +0300

    Create new main action intent in constructor.

--- java/com/cyanogenmod/paz00_keyHandler/KeyHandler.java
+++ java/com/cyanogenmod/paz00_keyHandler/KeyHandler.java
@@ -1,5 +1,9 @@
 package com.cyanogenmod.paz00kh;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
 
@@ -8,6 +12,18 @@ import com.android.internal.os.DeviceKeyHandler;
 
 public final class KeyHandler implements DeviceKeyHandler {
 	private static final String TAG = "paz00_KeyHandler";
+
+	private final Context mContext;
+	private final Intent mSettingsIntent;
+
+	public KeyHandler(Context context) {
+		mContext = context;
+
+		mSettingsIntent = new Intent(Intent.ACTION_MAIN, null);
+		mSettingsIntent.setAction(Settings.ACTION_SETTINGS);
+		mSettingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+				| Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+	}
 
 	@Override
 	public boolean handleKeyEvent(KeyEvent event) {
